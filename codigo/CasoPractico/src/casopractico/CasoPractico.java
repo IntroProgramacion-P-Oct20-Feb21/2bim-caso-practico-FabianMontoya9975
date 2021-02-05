e3af127d11cde2f053e4a0dd8210b13369c04ef8
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2021-02-04 22:52:44 -0500

    Resolucion problematica en Java

--- codigo/CasoPractico/src/casopractico/CasoPractico.java
+++ codigo/CasoPractico/src/casopractico/CasoPractico.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Generar un solución en lenguaje de programación Java
+    Que permita ingresar nuevas cuentas de diversas plataformas.
  */
 package casopractico;
 
